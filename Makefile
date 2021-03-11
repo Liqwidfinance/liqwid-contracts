2454b456abf2cb05d210354523275958b8611e05
Tilde Rose <tilde@mlabs.city>
2021-03-11 17:39:41 +0000

    pab server via nix-shell

--- Makefile
+++ Makefile
@@ -1,2 +1,22 @@
-hoogle: shell.nix
-	nix-shell shell.nix --run 'hoogle server --local'
+# This makefile should be
+# called from inside the nix-shell environment
+#
+hoogle:
+	hoogle server --local
+
+pab_servers: pab_db install_contracts
+	@echo "Starting plutus servers."
+	plutus-pab --config=${PAB_CONFIG_FILE} all-servers
+
+pab_db:
+	@echo "Generating pab db at ${PAB_DB_PATH}"
+	plutus-pab --config=${PAB_CONFIG_FILE} migrate
+
+install_contracts: pab_db contracts
+# plutus-pab --config=${PAB_CONFIG_FILE} contracts install STACK OUTPUT GOES HERE
+
+contracts: liquid-contracts.cabal
+	stack build
+
+liquid-contracts.cabal: package.yaml
+	hpack -f
--- shell.nix
+++ shell.nix
@@ -4,10 +4,9 @@ let
   #haskellNix_src = builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz;
   #haskellNix = (import haskellNix_src) {};
 
-  ### Pin nixpkgs 20.09 with haskellNix overlays and config added
-  #nixpkgs_src = haskellNix.sources.nixpkgs2009;
+  #### Pin nixpkgs 20.09 with haskellNix overlays and config added
+  #nixpkgs_src = haskellNix.sources.nixpkgs-unstable;
   #nixpkgs = (import nixpkgs_src) haskellNix.nixpkgsArgs;
-
   bootstrap = import <nixpkgs> {} ;
 
   # Plutus
@@ -15,25 +14,50 @@ let
   plutus_src = bootstrap.fetchFromGitHub {
     owner = "input-output-hk";
     repo = "plutus";
+    fetchSubmodules = true;
+    leaveDotGit = true;
     inherit (plutus_git) rev sha256;
   };
 
   plutus = (import plutus_src) {};
 
+  plutus_pab_exe = plutus.plutus-pab.pab-exes.plutus-pab;
 
+  plutus_pab_client    = plutus.plutus-pab.client;
+  plutus_pab_conf      = (import ./pab_conf.nix) // { client = plutus_pab_client; };
+  plutus_pab_conf_file = plutus.plutus-pab.mkConf plutus_pab_conf;
 
 in
 
+# Maybe it would be easier to just call the nix shell in plutus/shell.nix ...
 plutus.plutus.haskell.project.shellFor {
   # Select packages who's dependencies should be added to the shell env
   packages = ps : [];
 
   # Select packages which should be added to the shell env, with their dependencies
+  # Should try and get the extra cardano dependencies in here...
   additional = ps: with ps; [
     plutus-pab plutus-tx plutus-tx-plugin
     plutus-contract
     plutus-ledger plutus-ledger-api
+    plutus-core
     playground-common
+    prettyprinter-configurable
+    freer-extras
+
+    # cardano-base
+    cardano-binary
+    cardano-slotting
+
+    cardano-crypto
+
+    # cardano-prelude
+    cardano-prelude
+    cardano-prelude-test
+
+    # ouroboros-network
+    ouroboros-network
+    ouroboros-network-framework
   ];
 
   # Local hoogle index of all packages
@@ -42,14 +66,20 @@ plutus.plutus.haskell.project.shellFor {
   # Extra haskell tools (arg passed on to mkDerivation)
   # Using the plutus.pkgs to use nixpkgs version from plutus (nixpkgs-unstable, mostly)
   propagatedBuildInputs = with plutus.pkgs; [
+    # Haskell Tools
     ormolu stack hlint haskell-language-server
+
+    # Pab
+    plutus_pab_exe plutus_pab_client
+  ];
+
+  buildInputs = [
+
   ];
 
 
- #tools = {
- #   stack  = { version = "2.5.1.1";   };
- #   ormolu = { version = "0.1.2.0"; };
- #   hlint  = { version = "3.2.7";   };
- # };
+  PAB_CONFIG_FILE = plutus_pab_conf_file;
+  PAB_CLIENT_PATH = plutus_pab_client;
+  PAB_DB_PATH     = plutus_pab_conf.db-file;
 
 }
